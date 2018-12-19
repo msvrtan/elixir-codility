6ad3823f9d74aaaf52beb18a3ddb8e2e4e680f22
Miro Svrtan <miro@mirosvrtan.me>
2018-12-19 23:27:08 +0100

    Feat #01: Add codility project

---
+++ codility/lib/codility.ex
@@ -0,0 +1,18 @@
+defmodule Codility do
+  @moduledoc """
+  Documentation for Codility.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Codility.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ codility/test/codility_test.exs
@@ -0,0 +1,8 @@
+defmodule CodilityTest do
+  use ExUnit.Case
+  doctest Codility
+
+  test "greets the world" do
+    assert Codility.hello() == :world
+  end
+end
