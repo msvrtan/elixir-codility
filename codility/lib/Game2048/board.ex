88a577a4dd98f3883a0a338ffb810d30b68a1cda
Miro Svrtan <miro@mirosvrtan.me>
2018-12-21 14:19:30 +0100

    Feat #5: Add Board where 2048 game will be played

---
+++ codility/lib/Game2048/board.ex
@@ -0,0 +1,5 @@
+defmodule Game2048.Board do
+  @moduledoc """
+  Documentation for 2048 board .
+  """
+end
---
+++ codility/test/Game2048/board_test.exs
@@ -0,0 +1,5 @@
+defmodule Game2048.BoardTest do
+  use ExUnit.Case
+  doctest Game2048.Board
+  alias Game2048.Board
+end
