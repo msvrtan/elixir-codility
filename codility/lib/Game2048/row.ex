35cd4883010cf90fc08cb3c668238a1170a0c7f8
Miro Svrtan <miro@mirosvrtan.me>
2018-12-21 17:51:29 +0100

    Feat #5: Support left on row

--- codility/lib/Game2048/row.ex
+++ codility/lib/Game2048/row.ex
@@ -6,4 +6,33 @@ defmodule Game2048.Row do
   def empty(size) do
     for _ <- 1..size, do: nil
   end
+
+  #
+  def left(row) do
+    go_left(row)
+    |> squash()
+  end
+
+  # Moves nil to the right
+  defp go_left([nil | tail]) do
+    go_left(tail) ++ [nil]
+  end
+
+  # Keeps value and recurses through tail
+  defp go_left([head | tail]) do
+    [head] ++ go_left(tail)
+  end
+
+  # When there is nothing more in the list
+  defp go_left([]), do: []
+
+  defp squash([head | [next | tail]]) when head == next and head != nil do
+    [head + next] ++ squash(tail) ++ [nil]
+  end
+
+  defp squash([head | tail]) do
+    [head] ++ squash(tail)
+  end
+
+  defp squash([]), do: []
 end
